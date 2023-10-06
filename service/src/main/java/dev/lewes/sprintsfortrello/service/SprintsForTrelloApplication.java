b6109a64f0d176e30233a5bdc813a3748ae6e467
Lewes <me@lewes.dev>
2023-10-06 12:39:25 +0100

    Add Spring quickstart

---
+++ service/src/main/java/dev/lewes/sprintsfortrello/service/SprintsForTrelloApplication.java
@@ -0,0 +1,13 @@
+package dev.lewes.sprintsfortrello.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintsForTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintsForTrelloApplication.class, args);
+	}
+
+}
