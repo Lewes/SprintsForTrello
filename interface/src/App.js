0b93b335693337f131b117e25cea61eac6abb10e
Lewes <me@lewes.dev>
2023-10-13 13:38:24 +0100

    Rough initial work on an interface

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+function CreateSprintButton(props) {
+    const createSprint = () => {
+        fetch("http://localhost:8080/sprints", {method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: 'Sprint Test', current: true })})
+            .then(res => res.json())
+            .then(json => {
+                props.setSprint(json);
+
+                fetch("http://localhost:8080/tasks", {method: 'POST'});
+                fetch("http://localhost:8080/sprints/" + json.id + "/tasks", {method: 'POST'})
+                    .then(res => res.json())
+                    .then(json => {
+                        props.setSprint(json);
+                    });
+            });
+    };
+
+    if(props.sprint != null) {
+        return null;
+    }
+
+    return (<button
+        onClick={createSprint}
+        title="Learn More"
+        color="#841584"
+    >Create Sprint</button>);
+}
 
 function App() {
+  const [sprint, setSprint] = useState([]);
+
+  const hasCurrentSprint = () => {
+      return sprint != null;
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:8080/sprints/current", {method: 'GET'})
+        .then(res => {
+          if(!res.ok) {
+            throw new Error("No current sprint")
+          }
+
+          return res.json();
+        }).catch(error => {})
+        .then(json => setSprint(json))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CreateSprintButton sprint={sprint} setSprint={setSprint}></CreateSprintButton>
+
+      {JSON.stringify(sprint, null, 2)}
     </div>
   );
 }
